ca7ce7c19130d4d06eae5ab0d51debe6c003199f
Marcin <31159047+lazyspot@users.noreply.github.com>
2019-11-18 13:25:15 +0100

    Create setup.py

---
+++ georeference-image-library/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='georeference-image-library',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='LazySpot',
+    author_email='',
+    description=''
+)
