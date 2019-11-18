38bcd96397292583abba3b62f0ebc9adb1bbd6f7
Marcin <31159047+lazyspot@users.noreply.github.com>
2019-11-18 13:54:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from setuptools import setup
-import os
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='georeference-image-library',
-    version='1.0',
-    packages=[''],
-    url='',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/lazyspot/georeference-image-library',
     license='',
     author='LazySpot',
     author_email='',
-    description=''
+    description='',
 )
