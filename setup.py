6ed83cbee37901057ef8f075f8b744635fa378a4
Marcin <31159047+lazyspot@users.noreply.github.com>
2019-11-19 15:01:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setuptools.setup(
     version='0.0.1',
     description='Library for georeference image.',
     py_modules=["image"],
-	package_dir={'': 'georeference-image-library'},
     packages=setuptools.find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -16,7 +15,6 @@ setuptools.setup(
     license='',
     author='LazySpot',
     author_email='marcin@lazyspot.net',
-
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
